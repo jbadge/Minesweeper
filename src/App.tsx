e3366fab59b9b939e9f38282cb65e8b5877c596c
Jeremy Badger <jeremebadger@gmail.com>
2024-02-14 16:32:54 -0800

    Static Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <h1>
+        Minesweeper - <button>New</button>
+      </h1>
+      <button></button>
+    </div>
+  )
 }
