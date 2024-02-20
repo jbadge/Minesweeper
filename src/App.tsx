db1461eafa67473d4b963fb5d03831139b2e6705
Jeremy Badger <jeremebadger@gmail.com>
2024-02-20 13:05:20 -0800

    Added Cell.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Cell } from './components/Cell'
 
 type Square =
   | ' '
@@ -16,12 +17,13 @@ type Square =
   | '8'
 type Row = [Square, Square, Square, Square, Square, Square, Square, Square]
 type Board = [Row, Row, Row, Row, Row, Row, Row, Row]
+type Difficulty = 0 | 1 | 2
 type Game = {
   id: null | number
   board: Board
   state: null | string
   mines: null | number
-  difficulty: null | number
+  difficulty: Difficulty
 }
 
 export function App() {
@@ -39,8 +41,10 @@ export function App() {
     ],
     state: null,
     mines: null,
-    difficulty: null,
+    difficulty: 0,
   })
+
+  const [difficulty, setDifficulty] = useState<Difficulty>(0)
 
   async function handleClickCell(
     event: React.MouseEvent,
@@ -58,8 +62,6 @@ export function App() {
       return
     }
 
-    console.log({ row, col })
-
     if (event.nativeEvent.button === 0) {
       const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/check`
 
@@ -75,11 +77,7 @@ export function App() {
         const newGameState = (await response.json()) as Game
         setGame(newGameState)
       }
-    }
-
-    // async function handleRightClickCell(row: number, col: number) {
-    //   console.log({ row, col })
-    else if (event.nativeEvent.button === 2) {
+    } else if (event.nativeEvent.button === 2) {
       const url = `https://minesweeper-api.herokuapp.com/games/${game.id}/flag`
       const body = { row, col }
 
@@ -96,70 +94,74 @@ export function App() {
     }
   }
 
-  async function handleNewGame() {
+  async function handleNewGame(newGameDifficulty: Difficulty) {
+    const diff = { difficulty: newGameDifficulty }
+
     const response = await fetch(
       'https://minesweeper-api.herokuapp.com/games',
       {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(diff),
       }
     )
 
     if (response.ok) {
       const newGameState = (await response.json()) as Game
-      console.log(newGameState)
+      setDifficulty(newGameDifficulty)
       setGame(newGameState)
     }
   }
 
-  // const checkState = () => {
-  //   switch (game.state) {
-  //     case 'won':
-  //       return 'You won!'
-  //     case 'lost':
-  //       return 'You lost!'
-  //     default:
-  //       return 'Minesweeper'
-  //   }
-  // }
+  useEffect(() => {
+    handleNewGame(0)
+  }, [])
 
   function checkState() {
     if (game.state === 'new') {
-      return 'Starting new game'
+      return //'Starting new game'
     } else if (game.state === 'playing') {
       return 'Game in progress...'
-    } else if (game.state === 'new' || game.state === 'lost') {
+    } else if (game.state === 'won' || game.state === 'lost') {
       return `You ${game.state}!`
     }
   }
 
-  // const header = game.state ? checkState() : 'Minesweeper'
+  function isGameOver() {
+    if (game.state === 'won' || game.state === 'lost') {
+      return false
+    }
+    return true
+  }
 
   return (
     <div>
-      <h1>
-        Minesweeper
-        {/* {checkState()} - <button onClick={handleNewGame}>New</button> */}
-      </h1>
+      <h1>Minesweeper</h1>
       <h2>{checkState()}</h2>
-      <h3>
-        <button onClick={handleNewGame}>New</button>
-      </h3>
+
       <main>
-        <section className="difficulty-0">
+        <section
+          id={`difficulty-${difficulty}`}
+          className={isGameOver() ? undefined : 'game-over'}
+        >
           {game.board.map((row, rowIndex) =>
             row.map((col, colIndex) => (
-              <button
+              <Cell
                 key={colIndex}
-                onClick={(e) => handleClickCell(e, rowIndex, colIndex)}
-                onContextMenu={(e) => handleClickCell(e, rowIndex, colIndex)}
-              >
-                {game.board[rowIndex][colIndex]}
-              </button>
+                cell={game.board[rowIndex][colIndex]}
+                cellRowIndex={rowIndex}
+                cellColIndex={colIndex}
+                handleClickCell={handleClickCell}
+              />
             ))
           )}
         </section>
       </main>
+      <h3>
+        <button onClick={() => handleNewGame(0)}>Easy</button>
+        <button onClick={() => handleNewGame(1)}>Medium</button>
+        <button onClick={() => handleNewGame(2)}>Hard</button>
+      </h3>
     </div>
   )
 }
